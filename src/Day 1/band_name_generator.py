ffaabf8f08bab96121424327b7f6148163315e60
Thyago Chrystiann <thyago.chrystiann@hotmail.com>
2023-11-14 00:37:36 -0300

    Day One: Exercising input and output functions, and string concatenation.

---
+++ src/Day 1/band_name_generator.py
@@ -0,0 +1,6 @@
+# Exercise Day 1: Band Name Generator
+
+print("Welcome to the band name generator.")
+city = input("What's the name of the city you grow up? ")
+pet = input("What's your pet's name? ")
+print("Your band name could be " + city + " " + pet + ".")
